b8e17360e28410050ca3adfddb0135e1dfa59bd4
AxsPlayer <skyprince213@gmail.com>
2018-09-02 09:00:52 +0800

    Update auto_fe_test.py

--- auto-FE/auto_fe_test.py
+++ auto-FE/auto_fe_test.py
@@ -4,3 +4,42 @@
 This script is designed for unit test for all the functions in automatic feature engineering.
 """
 # Import necessary packages.
+import logging
+import os
+import unittest
+
+import data_clean
+import feature_engineering
+import feature_selection
+import step_by_step
+import evaluation
+
+
+class TestFeatureEngineering(unittest.TestCase):
+    """Test functions in all the scripts corresponding to feature engineering.
+
+    Test each feature engineering function and the total automatic feature engineering
+    script.
+    """
+    def __init__(self, *args, **kwargs):
+        """Init the test class with all the attributes."""
+        super(TestFeatureEngineering, self).__init__(*args, **kwargs)
+        self.
+        self.
+
+    def fetch_data(self):
+
+    def test_data_clean(self):
+
+    def test_feature_engineering(self):
+
+    def test_feature_selection(self):
+
+    def test_step_by_step(self):
+
+    def test_evaluation(self):
+
+
+if __name__ == "__main__":
+    # Use 'python -m unittest auto_fe_test' in console for unit test.
+    unittest.main()
